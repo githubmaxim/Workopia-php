c73be04573cf52d724d91a1755812184bc1aeece
GithubMax <githubmax@ukr.net>
2024-05-08 13:50:01 +0300

    Created new repository

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+require '../helpers.php';
+
+require basePath('/App/views/home.view.php'); //не особое улучшение, "городить такой огород", вместо двух точек в начале пути. Но по правилам так нужно!
+//require '../App/views/home.view.php';
+?>
